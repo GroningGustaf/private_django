048ac1baa064f8b33f01d6d0fd1bac3bf7a7dc8b
gustafgroning <gustaf.groning@sigicom.com>
2022-08-02 09:30:44 +0200

    wrote new test!

--- test.py
+++ test.py
@@ -11,4 +11,9 @@ class testAddition(unittest.TestCase):
         expected = 12
         self.assertEqual(actual, expected)
 
+    def testSimpleTest(self):
+        a = 4
+        b = 7
+        self.assertEqual(11, (a + b))
+
 # To run unittests - python3 -m unittest simple_functions.py
