cb3edf91879c2dd5f416625cf7ca5fc7249cc860
gustafgroning <gustaf.groning@sigicom.com>
2022-08-02 09:26:11 +0200

    created test file

---
+++ test.py
@@ -0,0 +1,14 @@
+import unittest
+from ci_cd.simple_functions import *
+class testAddition(unittest.TestCase):
+    def testAdditionSuccess(self):
+        actual = addition(3, 8)
+        expected = 11
+        self.assertEqual(actual, expected)
+
+    def testAdditionFailed(self):
+        actual = addition(3, 8)
+        expected = 13 # incorrect
+        self.assertEqual(actual, expected)
+
+# To run unittests - python3 -m unittest simple_functions.py
